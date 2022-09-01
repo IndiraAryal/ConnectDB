6526d99dff322c02cad6b088de17c961208b6a0a
Indira Aryal <45324833+IndiraAryal@users.noreply.github.com>
2022-09-01 20:13:48 +0545

    Add files via upload

---
+++ connectsql.R
@@ -0,0 +1,13 @@
+pacman::p_load(RMySQL)
+library(DBI)
+
+# Edit dbConnect() call
+con <- dbConnect(RMySQL::MySQL(), 
+                 dbname = "tweater", 
+                 host = "courses.csrrinzqubik.us-east-1.rds.amazonaws.com", 
+                 port = 3306,
+                 user = "student",
+                 password = "datacamp")
+dbListTables(con)
+dbReadTable(con,"users")
+dbDisconnect(con)
